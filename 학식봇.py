de5c6fe0866a62d24803f672f83d1ff09d3ec25c
kaistlayner <layner@kaist.ac.kr>
2021-08-22 19:21:17 +0900

    테섭으로 메시지 변경

--- 학식봇.py
+++ 학식봇.py
@@ -4,8 +4,8 @@ from discord.ext import commands, tasks
 app = commands.Bot(command_prefix='알림')
 
 # 테스트 서버
-# serverId = 832286544496033843
-# chattingId = 832286544496033846
+testServerId = 832286544496033843
+testChattingId = 832286544496033846
 # 야영지 서버
 serverId = 540930626505932800
 chattingId = 878736864633315358
@@ -24,7 +24,7 @@ async def on_ready():
     print('다음으로 로그인합니다: ' + 'app.user.name')
     print('connection was successful')
     await app.change_presence(status=discord.Status.online, activity=None)
-    await app.get_guild(serverId).get_channel(chattingId).send(f'학식봇 등장! 현재 분({datetime.datetime.now().minute}분 같은거)는 넣지 마세요.. 고장납니다 ㅠㅠ)')
+    await app.get_guild(testServerId).get_channel(testChattingId).send(f'학식봇 등장! 현재 분({datetime.datetime.now().minute}분 같은거)는 넣지 마세요.. 고장납니다 ㅠㅠ)')
     alarmLst2.append(39)
 
 # TODO: 경뿌 알림이( 29분 59분)
@@ -48,22 +48,22 @@ async def 루프():
             await app.get_guild(serverId).get_channel(chattingId).send(f'10초 전 ({curMin+1}분)', tts=True)
             alarmLst.append(curMin)
             time.sleep(11)
-    elif curMin in alarmLst2:
-        if curHour % 2 == 0:
-            if curSec > 1:
-                alarmLst2.remove(curMin)
-                await app.get_guild(serverId).get_channel(chattingId).send(f'이미 {curMin}분 이야!', tts=True)
-                time.sleep(61-curSec)
-                alarmLst2.append(curMin)
-            else:
-                alarmLst2.remove(curMin)
-                await app.get_guild(serverId).get_channel(chattingId).send(f'{curMin}분!', tts=True)
-                time.sleep(29)
-                await app.get_guild(serverId).get_channel(chattingId).send(f'30초 전 ({curMin+1}분)', tts=True)
-                time.sleep(20)
-                await app.get_guild(serverId).get_channel(chattingId).send(f'10초 전 ({curMin+1}분)', tts=True)
-                alarmLst2.append(curMin)
-                time.sleep(11)
+    # elif curMin in alarmLst2:
+    #     if curHour % 2 == 0:
+    #         if curSec > 1:
+    #             alarmLst2.remove(curMin)
+    #             await app.get_guild(serverId).get_channel(chattingId).send(f'이미 {curMin}분 이야!', tts=True)
+    #             time.sleep(61-curSec)
+    #             alarmLst2.append(curMin)
+    #         else:
+    #             alarmLst2.remove(curMin)
+    #             await app.get_guild(serverId).get_channel(chattingId).send(f'{curMin}분!', tts=True)
+    #             time.sleep(29)
+    #             await app.get_guild(serverId).get_channel(chattingId).send(f'30초 전 ({curMin+1}분)', tts=True)
+    #             time.sleep(20)
+    #             await app.get_guild(serverId).get_channel(chattingId).send(f'10초 전 ({curMin+1}분)', tts=True)
+    #             alarmLst2.append(curMin)
+    #             time.sleep(11)
 
 @app.command()
 async def 추가(ctx, *input):
