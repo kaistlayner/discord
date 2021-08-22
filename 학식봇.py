340715323da2bfb2b23c602b5507bc892dceea26
kaistlayner <layner@kaist.ac.kr>
2021-08-22 18:37:01 +0900

    짝수시 알림 추가

--- 학식봇.py
+++ 학식봇.py
@@ -15,7 +15,9 @@ alarmLst = []
 alarmRunningLst = [] 
 alarmInfo = {}
 alarmInfo['alarmOn'] = True
+alarmInfo['alarmOn2'] = True
 alarmInfo['alarmCnt'] = 0
+alarmLst2 = [] 
 
 @app.event
 async def on_ready():
@@ -23,13 +25,15 @@ async def on_ready():
     print('connection was successful')
     await app.change_presence(status=discord.Status.online, activity=None)
     await app.get_guild(serverId).get_channel(chattingId).send(f'학식봇 등장! 현재 분({datetime.datetime.now().minute}분 같은거)는 넣지 마세요.. 고장납니다 ㅠㅠ)')
+    alarmLst2.append(39)
 
 # TODO: 경뿌 알림이( 29분 59분)
 @tasks.loop(seconds=1)
 async def 루프():
     curSec = datetime.datetime.now().second
     curMin = datetime.datetime.now().minute
-    if(curMin in alarmLst):
+    curHour = datetime.datetime.now().hour
+    if curMin in alarmLst:
         if curSec > 1:
             alarmLst.remove(curMin)
             await app.get_guild(serverId).get_channel(chattingId).send(f'이미 {curMin}분 이야!', tts=True)
@@ -44,12 +48,35 @@ async def 루프():
             await app.get_guild(serverId).get_channel(chattingId).send(f'10초 전 ({curMin+1}분)', tts=True)
             alarmLst.append(curMin)
             time.sleep(11)
+    elif curMin in alarmLst2:
+        if curHour % 2 == 0:
+            alarmLst2.remove(curMin)
+            await app.get_guild(serverId).get_channel(chattingId).send(f'{curMin}분!', tts=True)
+            time.sleep(29)
+            await app.get_guild(serverId).get_channel(chattingId).send(f'30초 전 ({curMin+1}분)', tts=True)
+            time.sleep(20)
+            await app.get_guild(serverId).get_channel(chattingId).send(f'10초 전 ({curMin+1}분)', tts=True)
+            alarmLst2.append(curMin)
+            time.sleep(11)
+
 
 @app.command()
 async def 추가(ctx, *input):
     for elem in input:
         num = (int)(elem)
-        if num not in alarmLst:
+        if num not in alarmLst and num not in alarmLst2:
+            if num > -1 and num < 60:
+                alarmLst.append(num)
+                alarmLst.sort()
+            else:
+                await ctx.send(f'{num}는 숫자 범위가 맞지 않습니다(0~59)')
+    await ctx.send(alarmLst)
+
+@app.command()
+async def 추가2(ctx, *input):
+    for elem in input:
+        num = (int)(elem)
+        if num not in alarmLst and num not in alarmLst2:
             if num > -1 and num < 60:
                 alarmLst.append(num)
                 alarmLst.sort()
@@ -107,6 +134,12 @@ async def 리스트(ctx):
     if(len(alarmLst) == 0):
         await ctx.send('등록된 알람이 없엉..')
     await ctx.send(f'{alarmLst}에 울려!')
+
+@app.command()
+async def 리스트2(ctx):
+    if(len(alarmLst2) == 0):
+        await ctx.send('등록된 알람이 없엉..')
+    await ctx.send(f'{alarmLst2}에 울려!')
 
 @app.command()
 async def 지금(ctx):
