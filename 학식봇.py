d0a3034e7a3b54d7ea6f047b4114cf0c10e86ee5
kaistlayner <layner@kaist.ac.kr>
2021-11-30 00:13:52 +0900

    [FEAT] 랜덤기능

--- 학식봇.py
+++ 학식봇.py
@@ -1,5 +1,6 @@
 import discord, asyncio, datetime, time
 from discord.ext import commands, tasks
+from random import *
 
 app = commands.Bot(command_prefix='알림')
 
@@ -12,6 +13,9 @@ app = commands.Bot(command_prefix='알림')
 # 메--창 서버
 메창서버 = 808742146391801856
 재획알리미 = 878999964351606784
+# 이슬
+이슬서버 = 815553588843118622
+재획알림방 = 914890809768304720
 
 alarmLst = [] 
 alarmLst2 = []
@@ -27,6 +31,7 @@ async def on_ready():
     # await app.get_guild(테스트서버).get_channel(일반채팅).send('학식봇 재시작!')
     await app.get_guild(야영지서버).get_channel(재획알림방).send('학식봇 재시작!')
     # await app.get_guild(메창서버).get_channel(재획알리미).send('학식봇 재시작!')
+    await app.get_guild(이슬서버).get_channel(재획알림방).send('학식봇 재시작!')
     # alarmLst2.append(39)
     del alarmLst[:]
     alarmLst.append(29)
@@ -74,122 +79,131 @@ async def 루프():
     #         return
 
 @app.command()
-async def 추가(ctx, *input):
-    for elem in input:
-        num = (int)(elem)
-        if num not in alarmLst and num not in alarmLst2:
-            if num > -1 and num < 60:
-                alarmLst.append(num)
-                alarmLst.sort()
-            else:
-                await ctx.send(f'{num}는 숫자 범위가 맞지 않습니다(0~59)')
-    await ctx.send(alarmLst)
+async def 랜덤(ctx, *input):
+    dstList = []
+    for person in input:
+        dstList.append(person)
+    await ctx.send(f'{dstList}중 당첨자는...')
+    length = len(dstList)
+    random = randrange(length)
+    await ctx.send(dstList[random])
 
-@app.command()
-async def 추가2(ctx, *input):
-    for elem in input:
-        num = (int)(elem)
-        if num not in alarmLst and num not in alarmLst2:
-            if num > -1 and num < 60:
-                alarmLst2.append(num)
-                alarmLst2.sort()
-            else:
-                await ctx.send(f'{num}는 숫자 범위가 맞지 않습니다(0~59)')
-    await ctx.send(alarmLst2)
+# async def 추가(ctx, *input):
+#     for elem in input:
+#         num = (int)(elem)
+#         if num not in alarmLst and num not in alarmLst2:
+#             if num > -1 and num < 60:
+#                 alarmLst.append(num)
+#                 alarmLst.sort()
+#             else:
+#                 await ctx.send(f'{num}는 숫자 범위가 맞지 않습니다(0~59)')
+#     await ctx.send(alarmLst)
 
-@app.command()
-async def 제거(ctx, *input):
-    for elem in input:
-        num = (int)(elem)
-        if num in alarmLst:
-            alarmLst.remove(num)
-    await ctx.send(alarmLst)
+# @app.command()
+# async def 추가2(ctx, *input):
+#     for elem in input:
+#         num = (int)(elem)
+#         if num not in alarmLst and num not in alarmLst2:
+#             if num > -1 and num < 60:
+#                 alarmLst2.append(num)
+#                 alarmLst2.sort()
+#             else:
+#                 await ctx.send(f'{num}는 숫자 범위가 맞지 않습니다(0~59)')
+#     await ctx.send(alarmLst2)
 
-@app.command()
-async def 제거2(ctx, *input):
-    for elem in input:
-        num = (int)(elem)
-        if num in alarmLst2:
-            alarmLst2.remove(num)
-    await ctx.send(alarmLst2)
+# @app.command()
+# async def 제거(ctx, *input):
+#     for elem in input:
+#         num = (int)(elem)
+#         if num in alarmLst:
+#             alarmLst.remove(num)
+#     await ctx.send(alarmLst)
 
-@app.command()
-async def 다제거(ctx):
-    del alarmLst[:]
-    await ctx.send(alarmLst)
+# @app.command()
+# async def 제거2(ctx, *input):
+#     for elem in input:
+#         num = (int)(elem)
+#         if num in alarmLst2:
+#             alarmLst2.remove(num)
+#     await ctx.send(alarmLst2)
 
-@app.command()
-async def 다제거2(ctx):
-    del alarmLst2[:]
-    await ctx.send(alarmLst2)
+# @app.command()
+# async def 다제거(ctx):
+#     del alarmLst[:]
+#     await ctx.send(alarmLst)
 
-@app.command()
-async def 켜기(ctx):
-    if not alarmInfo['alarmOn']:
-        alarmInfo['alarmOn'] = True
-        루프.start()
-        await ctx.send("알림이 켜졌습니다!")
-    else:
-        await ctx.send("이미 알림이 켜져있어..!")
+# @app.command()
+# async def 다제거2(ctx):
+#     del alarmLst2[:]
+#     await ctx.send(alarmLst2)
 
-@app.command()
-async def 끄기(ctx):
-    if alarmInfo['alarmOn']:
-        alarmInfo['alarmOn'] = False
-        루프.cancel()
-        await ctx.send("알림이 꺼졌습니다..!")
-    else:
-        await ctx.send("이미 알림이 꺼져있어..!")
+# @app.command()
+# async def 켜기(ctx):
+#     if not alarmInfo['alarmOn']:
+#         alarmInfo['alarmOn'] = True
+#         루프.start()
+#         await ctx.send("알림이 켜졌습니다!")
+#     else:
+#         await ctx.send("이미 알림이 켜져있어..!")
 
-@app.command()
-async def 켰니(ctx):
-    if alarmInfo['alarmOn']:
-        await ctx.send('응 켰어')
-    else :
-        await ctx.send('아니 껐어')
+# @app.command()
+# async def 끄기(ctx):
+#     if alarmInfo['alarmOn']:
+#         alarmInfo['alarmOn'] = False
+#         루프.cancel()
+#         await ctx.send("알림이 꺼졌습니다..!")
+#     else:
+#         await ctx.send("이미 알림이 꺼져있어..!")
 
-@app.command()
-async def 껐니(ctx):
-    if alarmInfo['alarmOn']:
-        await ctx.send('아니 켰어')
-    else :
-        await ctx.send('응 껐어')
+# @app.command()
+# async def 켰니(ctx):
+#     if alarmInfo['alarmOn']:
+#         await ctx.send('응 켰어')
+#     else :
+#         await ctx.send('아니 껐어')
 
-@app.command()
-async def 리스트(ctx):
-    if(len(alarmLst) == 0):
-        await ctx.send('등록된 알람이 없엉..')
-    await ctx.send(f'{alarmLst}에 울려!')
+# @app.command()
+# async def 껐니(ctx):
+#     if alarmInfo['alarmOn']:
+#         await ctx.send('아니 켰어')
+#     else :
+#         await ctx.send('응 껐어')
 
-@app.command()
-async def 리스트2(ctx):
-    if(len(alarmLst2) == 0):
-        await ctx.send('등록된 알람이 없엉..')
-    await ctx.send(f'{alarmLst2}에 울려!')
+# @app.command()
+# async def 리스트(ctx):
+#     if(len(alarmLst) == 0):
+#         await ctx.send('등록된 알람이 없엉..')
+#     await ctx.send(f'{alarmLst}에 울려!')
 
-@app.command()
-async def 지금(ctx):
-    cur = datetime.datetime.now()
-    curSec = cur.second
-    curMin = cur.minute
-    curHour = cur.hour + 9
-    curDay = cur.day
-    if curHour >= 24:
-        curHour -= 24
-        curDay += 1
-    await ctx.send(f'{curDay}일 {curHour}시 {curMin}분 {curSec}초야!')
+# @app.command()
+# async def 리스트2(ctx):
+#     if(len(alarmLst2) == 0):
+#         await ctx.send('등록된 알람이 없엉..')
+#     await ctx.send(f'{alarmLst2}에 울려!')
 
-@app.command()
-async def 재획용(ctx):
-    del alarmLst[:]
-    alarmLst.append(29)
-    alarmLst.append(59)
-    await ctx.send('재획용 29분 59분 알림이야!')
-    if not alarmInfo['alarmOn']:
-        # await ctx.send('알람이 꺼져있어! (#알림켜기#)')
-        alarmInfo['alarmOn'] = True
-        루프.start()
-        await ctx.send("알림이 켜졌습니다!")
-        # 함수 안에 함수 넣는 방법 아시는 분..?
+# @app.command()
+# async def 지금(ctx):
+#     cur = datetime.datetime.now()
+#     curSec = cur.second
+#     curMin = cur.minute
+#     curHour = cur.hour + 9
+#     curDay = cur.day
+#     if curHour >= 24:
+#         curHour -= 24
+#         curDay += 1
+#     await ctx.send(f'{curDay}일 {curHour}시 {curMin}분 {curSec}초야!')
+
+# @app.command()
+# async def 재획용(ctx):
+#     del alarmLst[:]
+#     alarmLst.append(29)
+#     alarmLst.append(59)
+#     await ctx.send('재획용 29분 59분 알림이야!')
+#     if not alarmInfo['alarmOn']:
+#         # await ctx.send('알람이 꺼져있어! (#알림켜기#)')
+#         alarmInfo['alarmOn'] = True
+#         루프.start()
+#         await ctx.send("알림이 켜졌습니다!")
+#         # 함수 안에 함수 넣는 방법 아시는 분..?
 
 app.run('ODc4Njc2NzYxODUwODA2MzUy.YSEpgQ.9QodKdVKGt5jinZNwxEUkkhsn-I')
