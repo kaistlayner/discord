685ae93fda864b2b170ccd611d0ba70414fb989f
kaistlayner <layner@kaist.ac.kr>
2021-12-01 00:49:34 +0900

    [FIX] 재획알림방 복구

--- 학식봇.py
+++ 학식봇.py
@@ -9,13 +9,13 @@ app = commands.Bot(command_prefix='밍')
 테스트채팅 = 914897810153930833
 # 야영지 서버
 야영지서버 = 540930626505932800
-재획알림방 = 878736864633315358
+야영지재획알림방 = 878736864633315358
 # 메--창 서버
 메창서버 = 808742146391801856
-재획알리미 = 878999964351606784
+메창재획알림방 = 878999964351606784
 # 이슬
 이슬서버 = 815553588843118622
-재획알림방 = 914890809768304720
+이슬재획알림방 = 914890809768304720
 
 alarmLst = [] 
 alarmLst2 = []
@@ -29,9 +29,9 @@ async def on_ready():
     print('connection was successful')
     await app.change_presence(status=discord.Status.online, activity=None)
     # await app.get_guild(테스트서버).get_channel(테스트채팅).send('학식봇 재시작!')
-    await app.get_guild(야영지서버).get_channel(재획알림방).send('학식봇 재시작!')
-    # await app.get_guild(메창서버).get_channel(재획알리미).send('학식봇 재시작!')
-    await app.get_guild(이슬서버).get_channel(재획알림방).send('학식봇 재시작!')
+    await app.get_guild(야영지서버).get_channel(야영지재획알림방).send('학식봇 재시작!')
+    # await app.get_guild(메창서버).get_channel(메창재획알림방).send('학식봇 재시작!')
+    await app.get_guild(이슬서버).get_channel(이슬재획알림방).send('학식봇 재시작!')
     # alarmLst2.append(39)
     del alarmLst[:]
     alarmLst.append(29)
@@ -53,32 +53,32 @@ async def 루프():
         curDay += 1
     if curMin in alarmLst:
         if curSec == 0:
-            await app.get_guild(야영지서버).get_channel(재획알림방).send(f'{curMin}분!', tts=True)
-            await app.get_guild(메창서버).get_channel(재획알리미).send(f'{curMin}분!', tts=True)
-            await app.get_guild(이슬서버).get_channel(재획알림방).send(f'{curMin}분!', tts=True)
+            await app.get_guild(야영지서버).get_channel(야영지재획알림방).send(f'{curMin}분!', tts=True)
+            await app.get_guild(메창서버).get_channel(메창재획알림방).send(f'{curMin}분!', tts=True)
+            await app.get_guild(이슬서버).get_channel(이슬재획알림방).send(f'{curMin}분!', tts=True)
             return
         if curSec == 30:
-            await app.get_guild(야영지서버).get_channel(재획알림방).send(f'30초 전 ({curMin+1}분)', tts=True)
-            await app.get_guild(메창서버).get_channel(재획알리미).send(f'30초 전 ({curMin+1}분)', tts=True)
-            await app.get_guild(이슬서버).get_channel(재획알림방).send(f'{curMin}분!', tts=True)
+            await app.get_guild(야영지서버).get_channel(야영지재획알림방).send(f'30초 전 ({curMin+1}분)', tts=True)
+            await app.get_guild(메창서버).get_channel(메창재획알림방).send(f'30초 전 ({curMin+1}분)', tts=True)
+            await app.get_guild(이슬서버).get_channel(이슬재획알림방).send(f'{curMin}분!', tts=True)
             return
         if curSec == 50:
-            await app.get_guild(야영지서버).get_channel(재획알림방).send(f'10초 전 ({curMin+1}분)', tts=True)
-            await app.get_guild(메창서버).get_channel(재획알리미).send(f'10초 전 ({curMin+1}분)', tts=True)
-            await app.get_guild(이슬서버).get_channel(재획알림방).send(f'{curMin}분!', tts=True)
+            await app.get_guild(야영지서버).get_channel(야영지재획알림방).send(f'10초 전 ({curMin+1}분)', tts=True)
+            await app.get_guild(메창서버).get_channel(메창재획알림방).send(f'10초 전 ({curMin+1}분)', tts=True)
+            await app.get_guild(이슬서버).get_channel(이슬재획알림방).send(f'{curMin}분!', tts=True)
             return
     # if (curMin in alarmLst2) and (curHour % 2 == 0):
     #     if curSec == 0:
-    #         await app.get_guild(야영지서버).get_channel(재획알림방).send(f'{curMin}분! 이벤트 버프 ', tts=True)
-    #         await app.get_guild(메창서버).get_channel(재획알리미).send(f'{curMin}분! 이벤트 버프 ', tts=True)
+    #         await app.get_guild(야영지서버).get_channel(야영지재획알림방).send(f'{curMin}분! 이벤트 버프 ', tts=True)
+    #         await app.get_guild(메창서버).get_channel(메창재획알림방).send(f'{curMin}분! 이벤트 버프 ', tts=True)
     #         return
     #     if curSec == 30:
-    #         await app.get_guild(야영지서버).get_channel(재획알림방).send(f'이벤트 버프 30초 전 ({curMin+1}분)', tts=True)
-    #         await app.get_guild(메창서버).get_channel(재획알리미).send(f'이벤트 버프 30초 전 ({curMin+1}분)', tts=True)
+    #         await app.get_guild(야영지서버).get_channel(야영지재획알림방).send(f'이벤트 버프 30초 전 ({curMin+1}분)', tts=True)
+    #         await app.get_guild(메창서버).get_channel(메창재획알림방).send(f'이벤트 버프 30초 전 ({curMin+1}분)', tts=True)
     #         return
     #     if curSec == 50:
-    #         await app.get_guild(야영지서버).get_channel(재획알림방).send(f'이벤트 버프 10초 전 ({curMin+1}분)', tts=True)
-    #         await app.get_guild(메창서버).get_channel(재획알리미).send(f'이벤트 버프 10초 전 ({curMin+1}분)', tts=True)
+    #         await app.get_guild(야영지서버).get_channel(야영지재획알림방).send(f'이벤트 버프 10초 전 ({curMin+1}분)', tts=True)
+    #         await app.get_guild(메창서버).get_channel(메창재획알림방).send(f'이벤트 버프 10초 전 ({curMin+1}분)', tts=True)
     #         return
 
 @app.command()
